c06cad7590da0d1548b8e0aea59f25ff7f2c0bdf
Sergey Mishin <smishin91@yandex.ru>
2023-11-26 14:51:57 +0300

    add commands

--- src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -2,7 +2,7 @@
 
 namespace Ordering.Application.Features.Orders.Queries.GetOrdersList;
 
-public class GetOrdersListQuery: IRequest
+public class GetOrdersListQuery: IRequest<List<OrdersVm>>
 {
     public string UserName { get; set; }
 
